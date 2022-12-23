16efcee70181b6921f5c5674ef8ecf8f6b254977
Bentozox <bentozox_youtube@hotmail.com>
2022-12-24 00:04:22 +0100

    Add file saving

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,76 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import { remark } from 'remark';
+import { admonitions } from 'remark-admonitions';
+import * as html from 'remark-html';
 import * as vscode from 'vscode';
+import * as path from 'path';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+
 export function activate(context: vscode.ExtensionContext) {
+	console.log('Démarrage !');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "fivemblueprint" is now active!');
+	// commandId
+	const SIDE_PREVIEW_COMMAND = 'remark.sidePreview';
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('fivemblueprint.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from FivemBluePrint!');
+	const disposableSidePreview = vscode.commands.registerCommand(SIDE_PREVIEW_COMMAND, async () => {
+			initMarkdownPreview(context);
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposableSidePreview);
+}
+
+async function initMarkdownPreview(context: vscode.ExtensionContext) {
+	const panel = vscode.window.createWebviewPanel(
+			// Webview id
+			'liveHTMLPreviewer',
+			// Webview title
+			'[Preview]',
+			// This will open the second column for preview inside editor
+			2,
+			{
+					// Enable scripts in the webview
+					enableScripts: true,
+					retainContextWhenHidden: true,
+					// And restrict the webview to only loading content from our extension's `assets` directory.
+					localResourceRoots: [vscode.Uri.file(path.join(context.extensionPath, 'assets'))]
+			}
+	);
+	console.log("ICI : " + JSON.stringify(vscode.window.activeTextEditor?.document.getText()));
+
+	await editFile("Contenu");
+	
+
+	/*
+	// Contenu du fichier
+	let bpFileContent : string = vscode.window.activeTextEditor?.document.getText();
+
+	// Affichage du contenu
+	panel.webview.html = vscode.window.activeTextEditor?.document.getText() ?? "RIEN";
+	*/
+}
+
+/**
+ * Permet d'assigner le contenu du fichier actif
+ * @param content Contenu a mettre dans le fichier
+ */
+async function editFile(content : string) {
+	let lineCount: number = vscode.window.activeTextEditor?.document.lineCount ?? 0;
+	const edit = new vscode.WorkspaceEdit();
+	edit.replace(vscode.window.activeTextEditor?.document.uri!, new vscode.Range(0, 0, lineCount, 0), content);
+	let success = await vscode.workspace.applyEdit(edit);
+	await vscode.workspace.saveAll();
+}
+
+function bluePrintHtml() : string {
+	return ``;
+}
+
+function markdownCompiler() {
+	const admonitionsOptions = {};
+	return remark()
+			.use(html, [])
+			.use(admonitions, admonitionsOptions);
 }
 
 // This method is called when your extension is deactivated
